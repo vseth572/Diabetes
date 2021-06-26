e046d19b4811c74bddb26cfd959038df845a8ac8
Vinayak <vseth572@gmail.com>
2021-06-26 22:22:13 +0530

    Model Completed 22:21 26/06/2021 Reference : https://towardsdatascience.com/logistic-regression-a-simplified-approach-using-python-c4bc81a87c31

--- model.py
+++ model.py
@@ -3,10 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 d1 = pd.read_csv("C:/Users/vs_00/Desktop/Internship/SAS/Diabetes/archive/diabetes.csv")
 # print(d1.head())
-print(d1.info())
+# print(d1.info())
 #
 # print(type(d1['Glucose']))
 #
@@ -21,10 +24,20 @@ imputer = SimpleImputer(missing_values =np.nan,strategy ='mean')
 # d1['Insulin'].fillna(d1['Insulin'].mean())
 # d1['BMI'].fillna(d1['BMI'].mean())
 
-print(d1['SkinThickness'].isnull().values.any())
+# print(d1['SkinThickness'].isnull().values.any())
 #printing the correlation matrix
 corrMatrix = d1.corr()
 
 ##plotting a heat map of correlation matrix
 # sn.heatmap(corrMatrix, annot=True)
 # plt.show()
+
+# print(sn.countplot(x='Outcome',data=d1))
+# plt.show()
+
+x_train,x_test,y_train,y_test = train_test_split(d1.drop('Outcome', axis=1),d1['Outcome'], test_size=0.3,random_state=101)
+
+logmodel = LogisticRegression(max_iter = 150)
+logmodel.fit(x_train,y_train)
+predictions = logmodel.predict(x_test)
+print(classification_report(y_test,predictions))
